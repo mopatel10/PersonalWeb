910aa64c80962b3d80fdefe7d5dcbd9aaf50dde0
Mohammed Patel <mohammed.patel10@dcmail.ca>
2024-12-07 15:35:39 -0500

    na

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -18,7 +18,7 @@ exports.handler = async (event) => {
   const replyToEmail = isValidEmail(email) ? email : 'no-reply@yourdomain.com';
 
   const emailContent = {
-    to: ['mohammed.h.p2003@gmail.com'], 
+    to: ['mohammed.h.p2003@gmail.com', 'malsinimasachchige@gmail.com'], 
     from: 'mohammed.h.p@hotmail.com',
     replyTo: email, 
     subject: `New Contact Form Submission from ${name}`,
